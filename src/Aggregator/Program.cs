627edaea9f338cf657aa7522ea1021dd795abe1b
tommyly <tommyly764@gmail.com>
2015-11-20 11:38:15 +0000

    adds topshelf and fixes configuration

--- src/Aggregator/Program.cs
+++ src/Aggregator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Aggregator.Services;
 using Microsoft.Owin.Hosting;
+using Topshelf;
 
 namespace Aggregator
 {
@@ -8,10 +9,38 @@ namespace Aggregator
     {
         static void Main(string[] args)
         {
+            HostFactory.Run(x =>                                 
+            {
+                x.Service<ErrorAggregationService>(s =>                        
+                {
+                    s.ConstructUsing(name => new ErrorAggregationService());     
+                    s.WhenStarted(aggregator => aggregator.Start());             
+                    s.WhenStopped(aggregator => aggregator.Stop());              
+                });
+
+                x.RunAsLocalSystem();                            
+
+                x.SetDescription("Apps Monitor Error Aggregation Service");        
+                x.SetDisplayName("AppsMonitor.ErrorAggregator");                       
+                x.SetServiceName("AppsMonitor.ErrorAggregator");                        
+            });
+        }
+    }
+
+    public class ErrorAggregationService
+    {
+        private IDisposable _apiHost;
+
+        public void Start()
+        {
             var config = new AggregatorConfiguration();
 
-            WebApp.Start<Startup>(config.AggregatorUrl);
-            Console.ReadKey();
+            _apiHost = WebApp.Start<Startup>(config.AggregatorUrl);
+        }
+
+        public void Stop()
+        {
+            _apiHost.Dispose();
         }
     }
 }
